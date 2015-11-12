c09a7854a61c9fcf964b97d5494397d38059ab60
leoszcarvalho <leonardo.souzas30@gmail.com>
2015-11-12 13:52:06 -0200

    Commit do final do Express

--- src/app.js
+++ src/app.js
@@ -3,14 +3,20 @@
 var express = require('express'),
 	  posts = require('./mock/posts.json');
 
+//Retorna um array de objetos de cada post do arquivo json com suas respectivas chaves
+var postsLists = Object.keys(posts).map(function(value) {
+							         return posts[value]})
+
 var app = express();
 
-app.use("/static", express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res){
+	var path = req.path;
+	res.locals.path = path;
 	res.render('index');
 });
 
@@ -18,12 +24,26 @@ app.get('/blog/:title?', function(req, res){
 	var title = req.params.title;
 	if (title === undefined) {
 		res.status(503);
-		res.send("This page is under construction!");
+		res.render('blog', {posts: postsLists})
 	} else {
 		var post = posts[title] || {};
 		res.render('post', { post: post});
 	}
 });
+
+app.get('/posts', function(req, res){
+		if(req.query.raw)
+		{
+				res.json(posts);
+		}
+		else
+		{
+				res.json(postsLists);
+		}
+
+		res.json(postsLists);
+
+})
 
 app.listen(3000, function() {
 	console.log("The frontend server is running on port 3000!");
